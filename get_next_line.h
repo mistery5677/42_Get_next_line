055539205ddc47b403b9623572058e926a2835ae
Miguel Alexandre Afonso Costa <miafonso@c1r8s2.42lisboa.com>
2024-04-22 18:13:09 +0100

    updated

--- get_next_line.h
+++ get_next_line.h
@@ -1,1 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#endif
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 char *get_next_line(int fd);
