2252c87f8e4e34b56548c998ed904019f5385b86
Miguel Alexandre Afonso Costa <miafonso@c1r8s2.42lisboa.com>
2024-04-26 18:17:32 +0100

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -1,95 +1,102 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-char *reset_buffer(char *buffer)
+char	*reset_buffer(char *buffer)
 {
-	int i;
-	char *end;
-	char *str;
+	char	*end;
+	char	*str;
+	int		i;
 
 	end = ft_strchr(buffer, '\n');
-	if(!end)
+	if (!end)
 	{
 		free(buffer);
-		return NULL;
+		return (NULL);
 	}
 	else
 		i = (end - buffer) + 1;
-	if(!buffer[i])
+	printf("end_str = %s\n end_char = %c\n", end, end[i]);
+	if (!buffer[i])
 	{
 		free(buffer);
-		return NULL;
+		return (NULL);
 	}
-	str = ft_substr(buffer, i , ft_strlen(buffer) - i);
+	str = ft_substr(buffer, i, ft_strlen(buffer) - i);
 	free(buffer);
 	if (!str)
-		return NULL;
-	return str;
+		return (NULL);
+	return (str);
 }
 
-char *get_line(char *buffer)
+char	*get_line(char *buffer)
 {
-	int i;
-	char *new_str;
-	char *end;
+	char	*new_str;
+	char	*end;
+	int		i;
 
 	end = ft_strchr(buffer, '\n');
 	i = (end - buffer) + 1;
 	new_str = ft_substr(buffer, 0, i);
-	if(!new_str)
+	if (!new_str)
 		return (NULL);
 	return (new_str);
 }
 
-char *scan_fd(char *str, int fd)
+char	*scan_fd(char *str, int fd)
 {
-	char *buffer;
-	int bytes;
+	char	*buffer;
+	int		bytes;
 
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if(!buffer)
-		return NULL;
+	if (!buffer)
+		return (NULL);
 	buffer[0] = '\0';
 	bytes = 1;
-	while(bytes > 0  && !ft_strchr(buffer, '\n'))
+	while (bytes > 0 && !ft_strchr(buffer, '\n'))
 	{
 		bytes = read(fd, buffer, BUFFER_SIZE);
-		if(bytes > 0)
+		if (bytes > 0)
 		{
 			buffer[bytes] = '\0';
 			str = ft_strjoin(str, buffer);
 		}
 	}
-	free (buffer);
+	free(buffer);
 	if (bytes == -1)
 	{
 		free(str);
-		return NULL;
+		return (NULL);
 	}
-	return str;
+	return (str);
 }
 
 char	*get_next_line(int fd)
-{	
-	static char *buffer;
-	char 	*str;
+{
+	static char	*buffer;
+	char		*str;
 
-	if(fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
-		return NULL;	
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
+		return (NULL);
 	buffer = scan_fd(buffer, fd);
 	if (!buffer)
-		return NULL;
+		return (NULL);
 	str = get_line(buffer);
 	buffer = reset_buffer(buffer);
-	return str;
+	return (str);
 }
 
-/* int main()
+int main()
 {
 	int txt = open("teste.txt", O_RDONLY);
-	char *teste = get_next_line(txt);
-	//char	*teste = get_next_line();
-	printf("final: %s\n", teste);
-	free(teste);
+	char *teste ;
+	teste = get_next_line(txt);
+	int count = 1;
+	while(teste)
+	{
+		printf("final: [%d]	%s\n", count, teste)	;
+		free(teste);
+		teste = get_next_line(txt);
+		count++;
+	}
 	close(txt);
-} */
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,21 @@
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 #endif
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
+# define BUFFER_SIZE 10
 #endif
 
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
 
 size_t	ft_strlen(char *str);
-char *ft_strjoin(char *s1, char *s2);
-char *get_line(char *buffer);
-char *scan_fd(char *str, int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_line(char *buffer);
+char	*scan_fd(char *str, int fd);
 char	*ft_strchr(char *s, int c);
-char *get_next_line(int fd);
-char	*ft_substr(char  *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_strdup(char *src);
-
